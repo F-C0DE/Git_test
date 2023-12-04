e98754999ec8a824e63f7a2a24c196428595da2e
F-C0DE <78675655+F-C0DE@users.noreply.github.com>
2023-12-04 13:08:04 -0600

    Adding mainpy fibonacci plot (#1)
    * Added a main file.
    
    * Updated readme about main file.
    
    * Plotting fibonacci

---
+++ main.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib
+
+print(fibonacci(10))
+
+plt.plot(fibonacci(100))
+plt.show()
